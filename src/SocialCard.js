32cf429c2a00e60dbcc5df265fe06648893735f0
Sarah Manning <sarahnmanning@gmail.com>
2018-07-10 19:04:22 -0400

    added basic layout for card

--- src/SocialCard.js
+++ src/SocialCard.js
@@ -3,8 +3,18 @@ import React, {Component} from 'react';
 class SocialCard extends Component {
     render() {
         return (
-            <div>
-                Something
+            <div className='SocialCard-container'>
+                <section className='SocialCard-image'>
+                    <img src='https://via.placeholder.com/250x250'/>
+                </section>
+                <section className='SocialCard-interactions'>
+                    <button className='SocialCard-button like'>
+                        Like
+                    </button>
+                    <button className='SocialCard-button dislike'>
+                        Dislike
+                    </button>
+                </section>
             </div>
         );
     }
