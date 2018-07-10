a37ff6363fee21ceb145bd9cd9f10e50a1da6199
Sarah Manning <sarahnmanning@gmail.com>
2018-07-10 19:26:39 -0400

    added the ability to like and dislike an image within the card

--- src/SocialCard.js
+++ src/SocialCard.js
@@ -1,17 +1,47 @@
 import React, {Component} from 'react';
 
 class SocialCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            likes: 0,
+            dislikes: 0,
+        };
+    }
+
+    likeIt() {
+        this.setState({
+            likes: this.state.likes + 1
+        })
+    }
+
+    hateIt() {
+        this.setState({
+            dislikes: this.state.dislikes + 1
+        })
+    }
+
     render() {
         return (
             <div className='SocialCard-container'>
+            <section className='SocialCard-summary'>
+                <span>
+                    Likes: {this.state.likes}
+                </span>
+                <span>
+                    Dislikes: {this.state.dislikes}
+                </span>
+            </section>
                 <section className='SocialCard-image'>
                     <img src='https://via.placeholder.com/250x250'/>
                 </section>
                 <section className='SocialCard-interactions'>
-                    <button className='SocialCard-button like'>
+                    <button className='SocialCard-button like'
+                            onClick={this.likeIt.bind(this)}>
                         Like
                     </button>
-                    <button className='SocialCard-button dislike'>
+                    <button className='SocialCard-button dislike'
+                            onClick={this.hateIt.bind(this)}>
                         Dislike
                     </button>
                 </section>
