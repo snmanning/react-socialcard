7f8ba0c8f218bc34a2f2ce5755486a70a3222add
Sarah Manning <sarahnmanning@gmail.com>
2018-07-12 18:29:05 -0400

    styled the header

--- src/header.js
+++ src/header.js
@@ -1,9 +1,10 @@
 /* eslint-disable */
 import React from 'react';
 import PropTypes from 'prop-types';
+import './header.css';
 
 const Header = (props) => 
-        <header>
+        <header className='Header-container'>
             <h1> {props.title} </h1>
         </header>
 
