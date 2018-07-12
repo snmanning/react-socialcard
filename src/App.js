e7215f12c730e3f3a92a9a6bc9ebf29207cb89f1
Sarah Manning <sarahnmanning@gmail.com>
2018-07-11 20:41:52 -0400

    created basic header component and imported into app.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@
 import React, { Component } from 'react';
 import SocialCard from './SocialCard';
 import './App.css';
+import Header from './header';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       imageUrls: [
-        'https://placekitten.com/250/250',
+        'https://loremflickr.com/250/250/kitten,black/all',
         'https://placekitten.com/251/250',
         'https://placekitten.com/250/251',
         'https://placekitten.com/250/249',
@@ -23,6 +24,7 @@ class App extends Component {
     const cards = this.state.imageUrls.map((url) => <SocialCard imageSrc={url} key= {url} />)
     return (
       <div className='App-container'>
+        <Header/>
         {cards}
       </div>
     );
---
+++ src/header.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+    render () {
+        return (
+            <header>
+                Stuff and Things
+            </header>
+        );
+    }
+}
+
+export default Header;
